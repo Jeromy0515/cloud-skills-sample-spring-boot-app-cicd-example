5716e20a9b6f81861ef024321cd6333200f513a8
Jeormy0515 <qkr07224@gmail.com>
2022-01-26 16:25:11 +0900

    add backup.sh and update appspec

---
+++ scripts/backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+BUILD_PATH=$(ls /home/ec2-user/*.jar)
+JAR_NAME=$(basename $BUILD_PATH)
+
+TIME_STAMP=$(date +%Y%m%d%H%M%S)
+mkdir /${TIME_STAMP}
+
+mv /home/ec2-user/${JAR_NAME} /${TIME_STAMP}/
+
+aws s3 cp /${TIME_STAMP} s3://pmj-backup-bucket/
